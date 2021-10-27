8f4976b64656d079d3e5f19b7cb4c721cf3c6ed9
Artyom Aralov <artyom.aralov@gmail.com>
2021-10-27 03:23:04 +0300

    Auth logic ready

--- senior-project-app/src/App.js
+++ senior-project-app/src/App.js
@@ -48,7 +48,7 @@ function App() {
                 <Route
                   path="/logout"
                   exact
-                  component={() => (<Login message="User Logged Out Successfully." />)}
+                  component={(props) => (<Login message="User Logged Out Successfully." {...props} />)}
                 />
 
                 {/* Authenticated paths */}
