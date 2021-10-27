d507c6219026e359578ae2701119527b49aea41f
Artyom Aralov <artyom.aralov@gmail.com>
2021-10-28 01:56:08 +0300

    Reformat code

--- cloud-gateway/src/main/java/com/artyomdev/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/artyomdev/cloudgateway/CloudGatewayApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class CloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
 
 }
